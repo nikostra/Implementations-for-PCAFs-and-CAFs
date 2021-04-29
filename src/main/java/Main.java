eaa96c20c852848ef2976b3930841cbd6dd06b95
nikostra <niko.stratil@gmail.com>
2021-04-29 16:58:57 +0200

    file reading working

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,20 @@
+import af.PCAF;
+import service.FileService;
+
+import java.io.FileNotFoundException;
+
 public class Main {
     public static void main(String[] args) {
         if(args == null || args.length != 2){
             throw new IllegalArgumentException("Args don't have correct length");
         }
-        System.out.println("");
+        try {
+            PCAF pcaf = FileService.parseInput(args[1]);
+        } catch (FileNotFoundException e) {
+            System.out.println("Input file not found");
+        }
+        System.out.println(args[0] + args[1]);
     }
     // args: Reductions to perform (1,2,3,4) filepath to input, filepath to output?
+    // assumptions: txt als input und erst args dann prefs und attacks
 }
--- src/main/java/service/FileService.java
+++ src/main/java/service/FileService.java
@@ -1,4 +1,23 @@
 package service;
 
+import af.PCAF;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class FileService {
+
+
+    public static PCAF parseInput(String filepath) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File(filepath)).useDelimiter("\\.");
+        while (scanner.hasNext()){
+            String s = scanner.next().replaceAll("[\\n\\t ]", "");
+
+
+        }
+        return null;
+    }
 }
