9d4fe0af5b5f3c48b9ba2323e1ebb48bd6536fab
nikostra <niko.stratil@gmail.com>
2021-06-24 15:11:27 +0200

    implemented transitive check and new cl options

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,27 +7,71 @@ import util.ProtocolException;
 import util.VerificationException;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
+    /**
+     *
+     * @param args argument format: mandatory: (input filepath) (r1/r2/r3/r4 to select the reduction to perform)
+     *             Other options in any order: -wfi (for well-formed checks of input) -wfo (for well-formed check
+     *             of output) -vp (to verify preferences (transitive, irreflexive, asymmetric))
+     *
+     *             eg a.txt r1 -vp -wfo --> perform reduction 1 on a.txt and verify the preferences
+     *             and perform a well-formed check on the output
+     *
+     * @throws ProtocolException if there is an error while parsing the input
+     * @throws FileNotFoundException if the input file can not be found
+     * @throws VerificationException if verification fails
+     */
     public static void main(String[] args) throws ProtocolException, FileNotFoundException, VerificationException {
-        if(args == null || args.length != 2){
-            throw new IllegalArgumentException("Illegal arguments");
+        if(args == null){
+
+            throw new IllegalArgumentException("Illegal arguments, arguments cannot be empty");
         }
         FileService f = new FileService();
-        PCAF pcaf = f.parseInput(args[1]);
+        PCAF pcaf = f.parseInput(args[0]);
 
-        VerificationService.verifyPreferences(pcaf);
-        VerificationService.verifyWellFormedness(pcaf);
+        var argsList = Arrays.asList(args);
+        argsList = new ArrayList<>(argsList);
+        argsList.remove(0);
+
+        if(argsList.contains("-vp")){
+            VerificationService.verifyPreferences(pcaf);
+            argsList.remove("-vp");
+        }
+        if(argsList.contains("-wfi")){
+
+            if (VerificationService.verifyWellFormedness(pcaf)) {
+                System.out.println("Input is well-formed");
+            } else {
+                System.out.println("Input is NOT well formed");
+            }
+            argsList.remove("-wfi");
+        }
 
         ReductionService r = new ReductionService();
-        CAF caf = switch (args[0]) {
-            case "1" -> r.reduction1(pcaf);
-            case "2" -> r.reduction2(pcaf);
-            case "3" -> r.reduction3(pcaf);
-            case "4" -> r.reduction4(pcaf);
+        String reduction = argsList.get(0);
+        argsList.remove(0);
+        CAF caf = switch (reduction) {
+            case "r1" -> r.reduction1(pcaf);
+            case "r2" -> r.reduction2(pcaf);
+            case "r3" -> r.reduction3(pcaf);
+            case "r4" -> r.reduction4(pcaf);
             default -> throw new IllegalArgumentException("Illegal reduction argument");
         };
 
+        if(argsList.contains("-wfo")){
+            if (VerificationService.verifyWellFormedness(caf)) {
+                System.out.println("Output is well-formed");
+            } else {
+                System.out.println("Output is NOT well formed");
+            }
+            argsList.remove("-wfo");
+        }
+
+        System.out.println();
+        System.out.println("Output CAF:");
         f.parseOutput(caf);
     }
 }
--- src/main/java/service/VerificationService.java
+++ src/main/java/service/VerificationService.java
@@ -18,22 +18,43 @@ public class VerificationService {
             //asymmetric
             if (pcaf.getPreferences().contains(new Preference(preference.getInferior(), preference.getSuperior())))
                 throw new VerificationException("Preference not asymmetric: " + preference);
+        }
 
-            //transitive
+        //transitive
+        for (int i = 0; i < pcaf.getArguments().size(); i++) {
+            for (int j = i + 1; j < pcaf.getArguments().size(); j++) {
+                for (int k = j + 1; k < pcaf.getArguments().size(); k++) {
+                    Argument a = pcaf.getArguments().get(i);
+                    Argument b = pcaf.getArguments().get(j);
+                    Argument c = pcaf.getArguments().get(k);
 
+                    if (pcaf.getPreferences().contains(new Preference(b, a)) && pcaf.getPreferences().contains(new Preference(c, b)) && !pcaf.getPreferences().contains(new Preference(c, a)))
+                        throw new VerificationException("Preferences not transitive. Preference missing: " + new Preference(c, a));
+                    if (pcaf.getPreferences().contains(new Preference(c, a)) && pcaf.getPreferences().contains(new Preference(b, c)) && !pcaf.getPreferences().contains(new Preference(b, a)))
+                        throw new VerificationException("Preferences not transitive. Preference missing: " + new Preference(b, a));
+                    if (pcaf.getPreferences().contains(new Preference(a, b)) && pcaf.getPreferences().contains(new Preference(c, a)) && !pcaf.getPreferences().contains(new Preference(c, b)))
+                        throw new VerificationException("Preferences not transitive. Preference missing: " + new Preference(c, b));
+                    if (pcaf.getPreferences().contains(new Preference(a, c)) && pcaf.getPreferences().contains(new Preference(b, a)) && !pcaf.getPreferences().contains(new Preference(b, c)))
+                        throw new VerificationException("Preferences not transitive. Preference missing: " + new Preference(b, c));
+                    if (pcaf.getPreferences().contains(new Preference(b, c)) && pcaf.getPreferences().contains(new Preference(a, b)) && !pcaf.getPreferences().contains(new Preference(a, c)))
+                        throw new VerificationException("Preferences not transitive. Preference missing: " + new Preference(a, c));
+                    if (pcaf.getPreferences().contains(new Preference(c, b)) && pcaf.getPreferences().contains(new Preference(a, c)) && !pcaf.getPreferences().contains(new Preference(a, b)))
+                        throw new VerificationException("Preferences not transitive. Preference missing: " + new Preference(a, b));
+                }
+            }
         }
     }
 
-    public static void verifyWellFormedness(CAF caf){
+    public static boolean verifyWellFormedness(CAF caf) {
         // loop durch alle attacks, speicher jeweils ab welche claim und arg welches arg angreift. Am Ende wird für jedes gespeicherte arg überprüft ob es die selben angreift wie der claim
         List<Claim> claims = new ArrayList<>();
         List<Argument> args = new ArrayList<>();
 
         for (Attack attack : caf.getAttacks()) {
             var index = claims.indexOf(attack.getAttacker().getClaim());
-            if(index != -1){
+            if (index != -1) {
                 var claim = claims.get(index);
-                if(!claim.getAttacking().contains(attack.getDefender())) {
+                if (!claim.getAttacking().contains(attack.getDefender())) {
                     claim.getAttacking().add(attack.getDefender());
                 }
             } else {
@@ -43,9 +64,9 @@ public class VerificationService {
                 claims.add(claim);
             }
             index = args.indexOf(attack.getAttacker());
-            if(index != -1){
+            if (index != -1) {
                 var arg = args.get(index);
-                if(!arg.getAttacking().contains(attack.getDefender()))
+                if (!arg.getAttacking().contains(attack.getDefender()))
                     arg.getAttacking().add(attack.getDefender());
             } else {
                 var arg = attack.getAttacker();
@@ -58,23 +79,18 @@ public class VerificationService {
         boolean wellFormed = true;
 
         for (Argument argument : caf.getArguments()) {
-            if(!args.contains(argument) && claims.contains(argument.getClaim())) {
+            if (!args.contains(argument) && claims.contains(argument.getClaim())) {
                 wellFormed = false;
                 break;
             }
             var index = claims.indexOf(argument.getClaim());
-            if(index != -1){
+            if (index != -1) {
                 if (!new HashSet<>(argument.getAttacking()).equals(new HashSet<>(claims.get(index).getAttacking()))) {
                     wellFormed = false;
                     break;
                 }
             }
         }
-        System.out.println("Claims: ");
-        System.out.println(claims);
-        System.out.println("Args: ");
-        System.out.println(args);
-        System.out.println("Well-formed? " + wellFormed);
-        System.out.println();
+        return wellFormed;
     }
 }
